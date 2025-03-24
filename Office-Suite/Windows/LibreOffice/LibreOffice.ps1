3291b98f488980304f2b917cca092a7822e110be
Unikat-Cloud.Gitea <gitea@unikat-cloud.de>
2025-03-24 14:48:16 +0000

    Office-Suite/Windows/LibreOffice/LibreOffice.ps1 hinzugefügt

---
+++ Office-Suite/Windows/LibreOffice/LibreOffice.ps1
@@ -0,0 +1,20 @@
+# Installationsskript für LibreOffice
+
+# --- Chocolatey Installation (Auskommentierung entfernen, um zu verwenden) ---
+# Write-Host "LibreOffice wird mit Chocolatey installiert..."
+# choco install libreoffice-fresh -y
+# Write-Host "LibreOffice wurde mit Chocolatey installiert."
+# Write-Host "Chocolatey Installation abgeschlossen!"
+# ------------------------------------------------------------------------
+
+# --- Winget Installation (Auskommentierung entfernen, um zu verwenden) ---
+# Write-Host "LibreOffice wird mit Winget installiert..."
+# winget install LibreOffice -e --source winget --accept-package-agreements --accept-source-agreements
+# Write-Host "LibreOffice wurde mit Winget installiert."
+# Write-Host "Winget Installation abgeschlossen!"
+# ------------------------------------------------------------------------
+
+# --- Anmerkungen ---
+# Bitte entfernen Sie die Auskommentierung (#) vor der gewünschten Installationsmethode.
+# Führen Sie dieses Skript als Administrator aus.
+# -------------------
