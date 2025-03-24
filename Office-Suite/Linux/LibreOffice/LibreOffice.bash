f7fd2ed3f01699502d54d092aecaebfa4d225291
Unikat-Cloud.Gitea <gitea@unikat-cloud.de>
2025-03-24 14:45:16 +0000

    Office-Suite/Linux/LibreOffice/LibreOffice.bash hinzugefügt

---
+++ Office-Suite/Linux/LibreOffice/LibreOffice.bash
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# LibreOffice installieren
+echo "LibreOffice wird installiert..."
+flatpak install flathub org.libreoffice.LibreOffice -y
+echo "LibreOffice wurde installiert."
+
+echo "Installation abgeschlossen!"
