a91e9291aba92e96eaf0ed6316001569cedf56e6
Unikat-Cloud.Gitea <gitea@unikat-cloud.de>
2025-03-25 12:00:02 +0000

    Browser/Windows/Brave/Brave.ps1 hinzugefügt

---
+++ Browser/Windows/Brave/Brave.ps1
@@ -0,0 +1,15 @@
+# Installationsskript für Brave Browser
+
+# --- Chocolatey Installation (Auskommentierung entfernen, um zu verwenden) ---
+# Write-Host "Brave Browser wird mit Chocolatey installiert..."
+# choco install brave -y
+# Write-Host "Brave Browser wurde mit Chocolatey installiert."
+# Write-Host "Chocolatey Installation abgeschlossen!"
+# ------------------------------------------------------------------------
+
+# --- Winget Installation (aktiv) ---
+#Write-Host "Brave Browser wird mit Winget installiert..."
+#winget install BraveSoftware.BraveBrowser -e --source winget --accept-package-agreements --accept-source-agreements
+#Write-Host "Brave Browser wurde mit Winget installiert."
+#Write-Host "Winget Installation abgeschlossen!"
+# ------------------------------------------------------------------------
