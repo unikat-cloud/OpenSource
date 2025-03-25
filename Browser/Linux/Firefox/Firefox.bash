ff222f8c2258e8602caf8203895df8e2d1200c22
Unikat-Cloud.Gitea <gitea@unikat-cloud.de>
2025-03-25 11:56:59 +0000

    Browser/Linux/Firefox/Firefox.bash hinzugefügt

---
+++ Browser/Linux/Firefox/Firefox.bash
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Firefox installieren
+echo "Firefox wird installiert..."
+flatpak install flathub org.mozilla.firefox -y
+echo "Firefox wurde installiert."
+
+echo "Installation abgeschlossen!"
