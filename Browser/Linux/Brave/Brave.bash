4966813f3dcfba8bee08a8e64fd5f8e8b5ac67a5
Unikat-Cloud.Gitea <gitea@unikat-cloud.de>
2025-03-25 11:56:22 +0000

    Browser/Linux/Brave/Brave.bash hinzugefügt

---
+++ Browser/Linux/Brave/Brave.bash
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Brave Browser installieren
+echo "Brave Browser wird installiert..."
+flatpak install flathub com.brave.Browser -y
+echo "Brave Browser wurde installiert."
+
+echo "Installation abgeschlossen!"
