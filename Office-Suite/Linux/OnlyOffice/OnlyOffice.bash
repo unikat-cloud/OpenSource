8cc29b0c8d986e1cf2c9326af36cf009e2dc45e8
Unikat-Cloud.Gitea <gitea@unikat-cloud.de>
2025-03-28 22:01:31 +0000

    Office-Suite/Linux/OnlyOffice/OnlyOffice.bash hinzugefügt

---
+++ Office-Suite/Linux/OnlyOffice/OnlyOffice.bash
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# ONLYOFFICE installieren
+
+echo "ONLYOFFICE wird installiert..."
+flatpak install flathub org.onlyoffice.desktopeditors -y
+echo "ONLYOFFICE wurde installiert."
+
+echo "Installation abgeschlossen!"
