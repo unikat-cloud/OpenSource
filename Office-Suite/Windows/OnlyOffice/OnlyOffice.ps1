76dd4f4e42eb70d71ebc5cae606ee73cc1ff84d8
Unikat-Cloud.Gitea <gitea@unikat-cloud.de>
2025-03-28 22:03:23 +0000

    Office-Suite/Windows/OnlyOffice/OnlyOffice.ps1 hinzugefügt

---
+++ Office-Suite/Windows/OnlyOffice/OnlyOffice.ps1
@@ -0,0 +1,22 @@
+# Installationsskript für ONLYOFFICE
+
+# --- Chocolatey Installation (Auskommentierung entfernen, um zu verwenden) ---
+
+# Write-Host "ONLYOFFICE wird mit Chocolatey installiert..."
+# choco install onlyoffice-desktopeditors -y
+
+# Write-Host "ONLYOFFICE wurde mit Chocolatey installiert."
+# Write-Host "Chocolatey Installation abgeschlossen!"
+
+# --------------------------------------------------------------------------------------
+
+# --- Winget Installation (Auskommentierung entfernen, um zu verwenden) ---
+
+#Write-Host "ONLYOFFICE wird mit Winget installiert..."
+#winget install ONLYOFFICE -e --source winget --accept-package-agreements --accept-source-agreements
+
+#Write-Host "ONLYOFFICE wurde mit Winget installiert."
+
+#Write-Host "Winget Installation abgeschlossen!"
+
+# --------------------------------------------------------------------------------------
