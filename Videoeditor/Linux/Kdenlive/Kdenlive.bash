7b03530095b23e27f265f0e01b74b0b4709b4d73
Unikat-Cloud.Gitea <gitea@unikat-cloud.de>
2025-04-05 14:03:46 +0000

    Videoeditor/Linux/Kdenlive/Kdenlive.bash hinzugefügt

---
+++ Videoeditor/Linux/Kdenlive/Kdenlive.bash
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Kdenlive installieren
+echo 'Kdenlive wird installiert...'
+flatpak install flathub org.kde.kdenlive -y
+echo 'Kdenlive wurde installiert.'
+echo 'Installation abgeschlossen!'
