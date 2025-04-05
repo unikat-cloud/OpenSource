ad60bb9ae7f476b393f6425c17e7bee5eb5689a9
Unikat-Cloud.Gitea <gitea@unikat-cloud.de>
2025-04-05 14:06:17 +0000

    Videoeditor/Windows/Kdenlive/Kdenlive.ps1 hinzugefügt

---
+++ Videoeditor/Windows/Kdenlive/Kdenlive.ps1
@@ -0,0 +1,15 @@
+# Installationsskript für Kdenlive
+
+# --- Chocolatey Installation (Auskommentierung entfernen, um zu verwenden) ---
+# Write-Host "Kdenlive wird mit Chocolatey installiert..."
+# choco install kdenlive -y
+# Write-Host "Kdenlive wurde mit Chocolatey installiert."
+# Write-Host "Chocolatey Installation abgeschlossen"
+# -----------------------------------------------------------------------
+
+# --- Winget Installation (Auskommentierung entfernen, um zu verwenden) ---
+# Write-Host "Kdenlive wird mit Winget installiert..."
+# winget install Kdenlive -e --source winget --accept-package-agreements --accept-source-agreements
+# Write-Host "Kdenlive wurde mit Winget installiert."
+# Write-Host "Winget Installation abgeschlossen"
+# -----------------------------------------------------------------------
