e2e1a39d765a6d4b2f8093cf177a3538b1d34e90
Unikat-Cloud.Gitea <gitea@unikat-cloud.de>
2025-04-06 16:10:26 +0000

    Browser/Windows/Brave/Brave.ps1 aktualisiert

--- Browser/Windows/Brave/Brave.ps1
+++ Browser/Windows/Brave/Brave.ps1
@@ -1,6 +1,6 @@
 # Installationsskript für Brave Browser
 
 Write-Host "Brave Browser wird mit Winget installiert..."
-winget install BraveSoftware.BraveBrowser -e --source winget --accept-package-agreements --accept-source-agreements
+winget install Brave.Brave -e --source winget --accept-package-agreements --accept-source-agreements
 Write-Host "Brave Browser wurde mit Winget installiert."
 Write-Host "Winget Installation abgeschlossen!"
