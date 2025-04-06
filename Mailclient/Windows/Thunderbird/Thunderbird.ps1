5a8454b40577387868cfcfed225d0963267f26e1
Unikat-Cloud.Gitea <gitea@unikat-cloud.de>
2025-04-06 16:06:34 +0000

    Mailclient/Windows/Thunderbird/Thunderbird.ps1 aktualisiert

--- Mailclient/Windows/Thunderbird/Thunderbird.ps1
+++ Mailclient/Windows/Thunderbird/Thunderbird.ps1
@@ -1,15 +1,6 @@
 # Installationsskript für Thunderbird
 
-# --- Chocolatey Installation (Auskommentierung entfernen, um zu verwenden) ---
-# Write-Host "Thunderbird wird mit Chocolatey installiert..."
-# choco install thunderbird -y
-# Write-Host "Thunderbird wurde mit Chocolatey installiert."
-# Write-Host "Chocolatey Installation abgeschlossen!"
-# ------------------------------------------------------------------------
-
-# --- Winget Installation (aktiv) ---
-#Write-Host "Thunderbird wird mit Winget installiert..."
-#winget install Mozilla.Thunderbird -e --source winget --accept-package-agreements --accept-source-agreements
-#Write-Host "Thunderbird wurde mit Winget installiert."
-#Write-Host "Winget Installation abgeschlossen!"
-# ------------------------------------------------------------------------
+Write-Host "Thunderbird wird mit Winget installiert..."
+winget install Mozilla.Thunderbird -e --source winget --accept-package-agreements --accept-source-agreements
+Write-Host "Thunderbird wurde mit Winget installiert."
+Write-Host "Winget Installation abgeschlossen!"
