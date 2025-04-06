5594c5984c0dd9d28ebb5ae256b2d98aa70f584b
Unikat-Cloud.Gitea <gitea@unikat-cloud.de>
2025-04-06 16:05:05 +0000

    Audioeditor/Windows/Audacity/Audacity.ps1 aktualisiert

--- Audioeditor/Windows/Audacity/Audacity.ps1
+++ Audioeditor/Windows/Audacity/Audacity.ps1
@@ -1,15 +1,6 @@
 # Installationsskript für Audacity
 
-# --- Chocolatey Installation (Auskommentierung entfernen, um zu verwenden) ---
-# Write-Host "Audacity wird mit Chocolatey installiert.."
-# choco install audacity -y
-# Write-Host "Audacity wurde mit Chocolatey installiert."
-# Write-Host "Chocolatey Installation abgeschlossen!"
-# -----------------------------------------------------------------------
-
-# --- Winget Installation (Auskommentierung entfernen, um zu verwenden) ---
-#Write-Host "Audacity wird mit Winget installiert..."
-#winget install Audacity.Audacity -e --source winget --accept-package-agreements --accept-source-agreements
-#Write-Host "Audacity wurde mit Winget installiert."
-#Write-Host "Winget Installation abgeschlossen!"
-# -----------------------------------------------------------------------
+Write-Host "Audacity wird mit Winget installiert..."
+winget install Audacity.Audacity -e --source winget --accept-package-agreements --accept-source-agreements
+Write-Host "Audacity wurde mit Winget installiert."
+Write-Host "Winget Installation abgeschlossen!"
