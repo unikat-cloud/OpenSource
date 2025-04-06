e1c2a1ae3a49346b0c090963d108f74474d5e135
Unikat-Cloud.Gitea <gitea@unikat-cloud.de>
2025-04-06 10:51:58 +0000

    Audioeditor/Linux/Audacity/Audacity.bash hinzugefügt

---
+++ Audioeditor/Linux/Audacity/Audacity.bash
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Audacity installieren
+echo "Audacity wird installiert..."
+sudo apt update
+sudo apt install audacity -y
+echo "Audacity wurde installiert."
+
+echo "Installation abgeschlossen!"
