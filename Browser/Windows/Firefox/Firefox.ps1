51e6332b9d689c1ed8d4f2bee091a33cf6e89163
Unikat-Cloud.Gitea <gitea@unikat-cloud.de>
2025-04-06 16:06:03 +0000

    Browser/Windows/Firefox/Firefox.ps1 aktualisiert

--- Browser/Windows/Firefox/Firefox.ps1
+++ Browser/Windows/Firefox/Firefox.ps1
@@ -1,15 +1,6 @@
 # Installationsskript für Firefox
 
-# --- Chocolatey Installation (Auskommentierung entfernen, um zu verwenden) ---
-# Write-Host "Firefox wird mit Chocolatey installiert..."
-# choco install firefox -y
-# Write-Host "Firefox wurde mit Chocolatey installiert."
-# Write-Host "Chocolatey Installation abgeschlossen!"
-# ------------------------------------------------------------------------
-
-# --- Winget Installation (Auskommentierung entfernen, um zu verwenden) ---
-#Write-Host "Firefox wird mit Winget installiert..."
-#winget install Mozilla.Firefox -e --source winget --accept-package-agreements --accept-source-agreements
-#Write-Host "Firefox wurde mit Winget installiert."
-#Write-Host "Winget Installation abgeschlossen!"
-# ------------------------------------------------------------------------
+Write-Host "Firefox wird mit Winget installiert..."
+winget install Mozilla.Firefox -e --source winget --accept-package-agreements --accept-source-agreements
+Write-Host "Firefox wurde mit Winget installiert."
+Write-Host "Winget Installation abgeschlossen!"
