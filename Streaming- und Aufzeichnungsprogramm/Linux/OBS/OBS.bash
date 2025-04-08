2fa80c8a94d86717a37a994a9dda77c261478268
Unikat-Cloud.Gitea <gitea@unikat-cloud.de>
2025-04-08 10:59:25 +0000

    Streaming- und Aufzeichnungsprogramm/Linux/OBS/OBS.bash hinzugefügt

---
+++ Streaming- und Aufzeichnungsprogramm/Linux/OBS/OBS.bash
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# OBS installieren
+echo 'OBS wird installiert...'
+flatpak install flathub com.obsproject.Studio -y
+echo 'OBS wurde installiert.'
+echo 'Installation abgeschlossen!'
