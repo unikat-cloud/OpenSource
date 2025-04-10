19c204e3459431f32736191f0f508da2aea0c504
Unikat-Cloud.Gitea <gitea@unikat-cloud.de>
2025-04-10 11:48:07 +0000

    Vektorigrafikeditor/Linux/Inkscape/Inkscape.bash hinzugefügt

---
+++ Vektorigrafikeditor/Linux/Inkscape/Inkscape.bash
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Inkscape installieren
+echo 'Inkscape wird installiert...'
+flatpak install org.inkscape.Inkscape -y
+echo 'Inkscape wurde installiert.'
+echo 'Installation abgeschlossen!'
