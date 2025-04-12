716ca7a3466cc3d0630f94667688ce3ab76785cf
Unikat-Cloud.Gitea <gitea@unikat-cloud.de>
2025-04-12 12:23:00 +0000

    Passwordmanager/Bitwarden/Bitwarden.bash hinzugefügt

---
+++ Passwordmanager/Bitwarden/Bitwarden.bash
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Bitwarden installieren
+echo 'Bitwarden wird installiert...'
+flatpak install com.bitwarden.desktop -y
+echo 'Bitwarden wurde installiert.'
+echo 'Installation abgeschlossen!'
