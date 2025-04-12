d780e782a229cde360875e9f2b20c92f0bc41f8a
Unikat-Cloud.Gitea <gitea@unikat-cloud.de>
2025-04-12 12:23:32 +0000

    Passwordmanager/Proton Pass/Proton-Pass.bash hinzugefügt

---
+++ Passwordmanager/Proton Pass/Proton-Pass.bash
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Proton Pass installieren
+echo 'Proton Pass wird installiert...'
+flatpak install ch.protonmail.ProtonPass -y
+echo 'Proton Pass wurde installiert.'
+echo 'Installation abgeschlossen!'
