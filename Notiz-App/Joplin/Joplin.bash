50535d436c08369004e0d9dbff53dfef49403f87
Unikat-Cloud.Gitea <gitea@unikat-cloud.de>
2025-04-12 12:14:27 +0000

    Notiz-App/Joplin/Joplin.bash hinzugefügt

---
+++ Notiz-App/Joplin/Joplin.bash
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Joplin installieren
+echo 'Joplin wird installiert...'
+flatpak install flathub net.cozic.joplin_desktop -y
+echo 'Joplin wurde installiert.'
+echo 'Installation abgeschlossen!'
