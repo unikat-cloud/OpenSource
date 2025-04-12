e2f11983a1965f497a0239b3f72f83a6f65ee93c
Unikat-Cloud.Gitea <gitea@unikat-cloud.de>
2025-04-12 12:24:54 +0000

    Passwordmanager/Proton Pass/Proton-Pass.ps1 aktualisiert

--- Passwordmanager/Proton Pass/Proton-Pass.ps1
+++ Passwordmanager/Proton Pass/Proton-Pass.ps1
@@ -1,6 +1,6 @@
 # Installationsskript für Proton Pass
 
 Write-Host "Proton Pass wird mit Winget installiert..."
-winget install Proton.Pass -e --source winget --accept-package-agreements --accept-source-agreements
+winget install Proton.Proton.Pass -e --source winget --accept-package-agreements --accept-source-agreements
 Write-Host "Proton Pass wurde mit Winget installiert."
 Write-Host "Winget Installation abgeschlossen"
