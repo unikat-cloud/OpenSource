9a6ce3466c35d3c48b5f3a204c38e12a7a6167f1
Unikat-Cloud.Gitea <gitea@unikat-cloud.de>
2025-04-16 14:20:40 +0000

    Passwordmanager/KeepassXC/KeepassXC.ps1 hinzugefügt

---
+++ Passwordmanager/KeepassXC/KeepassXC.ps1
@@ -0,0 +1,6 @@
+# Installationsskript für keepass xc
+
+Write-Host "keepass xc wird mit Winget installiert..."
+winget install KeePassXCTeam.KeePassXC -e --source winget --accept-package-agreements --accept-source-agreements
+Write-Host "keepass xc wurde mit Winget installiert."
+Write-Host "Winget Installation abgeschlossen"
