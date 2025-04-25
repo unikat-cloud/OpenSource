2d948abb332e2a773eef565088fb73ef5f9eac4a
Unikat-Cloud.Gitea <gitea@unikat-cloud.de>
2025-04-25 17:35:17 +0000

    Notiz-App/Standard-Notes/Standard-Notes.ps1 hinzugefügt

---
+++ Notiz-App/Standard-Notes/Standard-Notes.ps1
@@ -0,0 +1,6 @@
+# Installationsskript für Standard Notes
+
+Write-Host "Standard Notes wird mit Winget installiert..."
+winget install StandardNotes.StandardNotes -e --source winget --accept-package-agreements --accept-source-agreements
+Write-Host "Standard Notes wurde mit Winget installiert."
+Write-Host "Winget Installation abgeschlossen"
