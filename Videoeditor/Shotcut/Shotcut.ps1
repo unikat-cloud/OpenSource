53abcf6c8c413ec6c0c31b97d89d182488bc8058
Unikat-Cloud.Gitea <selfhosted@unikat-cloud.de>
2025-04-30 20:58:45 +0000

    Videoeditor/Shotcut/Shotcut.ps1 hinzugefügt

---
+++ Videoeditor/Shotcut/Shotcut.ps1
@@ -0,0 +1,6 @@
+# Installationsskript für shotcut
+
+Write-Host "shotcut wird mit Winget installiert..."
+winget install Meltytech.Shotcut -e --source winget --accept-package-agreements --accept-source-agreements
+Write-Host "shotcut wurde mit Winget installiert."
+Write-Host "Winget Installation abgeschlossen"
