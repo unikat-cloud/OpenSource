c5d6082e048ddfc41f6488790fde659a3b043205
Unikat-Cloud.Gitea <selfhosted@unikat-cloud.de>
2025-04-30 20:59:53 +0000

    Videoeditor/Shotcut/Shotcut.bash hinzugefügt

---
+++ Videoeditor/Shotcut/Shotcut.bash
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Shotcut installieren
+echo 'Shotcut wird installiert...'
+flatpak install flathub org.shotcut.Shotcut -y
+echo 'Shotcut wurde installiert.'
+echo 'Installation abgeschlossen!'
