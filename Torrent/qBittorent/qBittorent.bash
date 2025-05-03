cc0deef3c23375427c9d42502c42b28588e78e6c
Unikat-Cloud.Gitea <selfhosted@unikat-cloud.de>
2025-05-03 18:37:27 +0000

    Torrent/qBittorent/qBittorent.bash hinzugefügt

---
+++ Torrent/qBittorent/qBittorent.bash
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# qBittorrent installieren
+echo 'qBittorrent wird installiert...'
+flatpak install flathub org.qbittorrent.qBittorrent -y
+echo 'qBittorrent wurde installiert.'
+echo 'Installation abgeschlossen!'
