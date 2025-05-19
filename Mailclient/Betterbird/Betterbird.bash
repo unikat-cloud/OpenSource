b04c065f90cd7e6fd7a89772a3c9862a72ae2334
Unikat-Cloud.Gitea <selfhosted@unikat-cloud.de>
2025-05-19 08:18:18 +0000

    Mailclient/Betterbird/Betterbird.bash hinzugefügt

---
+++ Mailclient/Betterbird/Betterbird.bash
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Betterbird installieren
+echo 'Betterbird wird installiert...'
+flatpak install flathub eu.betterbird.Betterbird -y
+echo 'Betterbird wurde installiert.'
+echo 'Installation abgeschlossen!'
