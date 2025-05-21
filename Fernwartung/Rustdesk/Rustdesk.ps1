a60b416db4d0d7a26b8adafb15a009540e069e17
Unikat-Cloud.Gitea <selfhosted@unikat-cloud.de>
2025-05-21 11:46:06 +0000

    Fernwartung/Rustdesk/Rustdesk.ps1 hinzugefügt

---
+++ Fernwartung/Rustdesk/Rustdesk.ps1
@@ -0,0 +1,6 @@
+# Installationsskript für rustdesk
+
+Write-Host "rustdesk wird mit Winget installiert..."
+winget install RustDesk.RustDesk -e --source winget --accept-package-agreements --accept-source-agreements
+Write-Host "rustdesk wurde mit Winget installiert."
+Write-Host "Winget Installation abgeschlossen"
