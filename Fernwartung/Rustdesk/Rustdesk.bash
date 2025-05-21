bfbf80c344fe7410517917d93c38472c85baa11b
Unikat-Cloud.Gitea <selfhosted@unikat-cloud.de>
2025-05-21 11:45:14 +0000

    Fernwartung/Rustdesk/Rustdesk.bash hinzugefügt

---
+++ Fernwartung/Rustdesk/Rustdesk.bash
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# rustdesk installieren
+echo 'rustdesk wird installiert...'
+flatpak install flathub com.rustdesk.RustDesk -y
+echo 'rustdesk wurde installiert.'
+echo 'Installation abgeschlossen!'
