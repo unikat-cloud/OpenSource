5dd4041781e87bc58b7c0ad7b9947000d00bfbb8
Unikat-Cloud.Gitea <selfhosted@unikat-cloud.de>
2025-05-28 08:42:06 +0000

    OS-Flasher/Balena-Etcher/Balena-Etcher.bash hinzugefügt

---
+++ OS-Flasher/Balena-Etcher/Balena-Etcher.bash
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# balenaEtcher installieren
+echo 'balenaEtcher wird installiert...'
+flatpak install flathub io.balena.Etcher -y
+echo 'balenaEtcher wurde installiert.'
+echo 'Installation abgeschlossen!'
