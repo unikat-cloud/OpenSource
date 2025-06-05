5f6a4e827065746e94fd93190bd00e223dbb790b
Unikat-Cloud.Gitea <selfhosted@unikat-cloud.de>
2025-06-05 08:34:44 +0000

    Zusatz/Ferdium/Ferdium.bash hinzugefügt

---
+++ Zusatz/Ferdium/Ferdium.bash
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Ferdium installieren
+echo 'Ferdium wird installiert...'
+flatpak install flathub org.ferdium.Ferdium -y
+echo 'Ferdium wurde installiert.'
+echo 'Installation abgeschlossen!'
