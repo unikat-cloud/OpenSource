f87ac53819650bed8ec28393552aff06a34de418
Unikat-Cloud <dev@unikat-cloud.de>
2026-08-23 18:50:36 +0200

    Erstelle Ventoy.ps1

---
+++ OS-Flasher/Ventoy/Ventoy.ps1
@@ -0,0 +1,6 @@
+# Installationsskript für Ventoy
+
+Write-Host "Ventoy wird mit Winget installiert..."
+winget install Ventoy.Ventoy -e --source winget --accept-package-agreements --accept-source-agreements
+Write-Host "Ventoy wurde mit Winget installiert."
+Write-Host "Winget Installation abgeschlossen!"
