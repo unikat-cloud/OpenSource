aacd2b41bb3696a3f164277a9e55ab77d4433baa
Unikat-Cloud <dev@unikat-cloud.de>
2026-08-23 19:02:30 +0200

    Erstelle Signal.bash

---
+++ Kommunikation/Signal/Signal.bash
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Signal installieren
+echo 'Signal wird installiert...'
+flatpak install flathub org.signal.Signal -y
+echo 'Signal wurde installiert.'
+echo 'Installation abgeschlossen!'
