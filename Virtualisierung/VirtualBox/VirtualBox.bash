2fc40c38e8b7e074f462d303e173b0b6cf4aa4a5
Unikat-Cloud <dev@unikat-cloud.de>
2026-08-23 19:02:51 +0200

    Erstelle VirtualBox.bash

---
+++ Virtualisierung/VirtualBox/VirtualBox.bash
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# VirtualBox installieren
+echo 'VirtualBox wird installiert...'
+
+sudo apt update
+sudo apt install -y virtualbox
+
+echo 'VirtualBox wurde installiert.'
+echo 'Installation abgeschlossen!'
