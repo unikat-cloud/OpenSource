26b52d5e6f9e2f8481479cf71d8b50943c947026
Unikat-Cloud <dev@unikat-cloud.de>
2026-08-23 15:41:48 +0200

    Erstelle Hermes-Desktop.ps1

---
+++ ki-tools/Hermes-Desktop/Hermes-Desktop.ps1
@@ -0,0 +1,6 @@
+# Installationsskript für Hermes Desktop
+
+Write-Host "Hermes Desktop wird mit Winget installiert..."
+winget install fathah.HermesDesktop -e --source winget --accept-package-agreements --accept-source-agreements
+Write-Host "Hermes Desktop wurde mit Winget installiert."
+Write-Host "Winget Installation abgeschlossen!"
