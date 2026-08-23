f618767e30c5f3b22a0dde411f2914401c31292a
Unikat-Cloud <dev@unikat-cloud.de>
2026-08-23 15:36:11 +0200

    Korrigiere Flatpak-ID für Proton Pass (ch.protonmail → me.proton)

--- Passwordmanager/Proton Pass/Proton-Pass.bash
+++ Passwordmanager/Proton Pass/Proton-Pass.bash
@@ -2,6 +2,6 @@
 
 # Proton Pass installieren
 echo 'Proton Pass wird installiert...'
-flatpak install ch.protonmail.ProtonPass -y
+flatpak install flathub me.proton.Pass -y
 echo 'Proton Pass wurde installiert.'
 echo 'Installation abgeschlossen!'
