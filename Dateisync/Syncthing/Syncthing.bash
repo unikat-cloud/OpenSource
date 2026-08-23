444b88a3000b853c25e6bf435af71f099ba0afc9
Unikat-Cloud <dev@unikat-cloud.de>
2026-08-23 19:03:10 +0200

    Erstelle Syncthing.bash

---
+++ Dateisync/Syncthing/Syncthing.bash
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Syncthing installieren
+echo 'Syncthing wird installiert...'
+
+sudo apt update
+sudo apt install -y syncthing
+
+echo 'Syncthing wurde installiert.'
+echo 'Installation abgeschlossen!'
