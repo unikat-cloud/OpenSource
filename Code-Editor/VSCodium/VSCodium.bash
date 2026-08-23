01d3d1f4e5b23210842a1bb68cc3a090b44b4774
Unikat-Cloud <dev@unikat-cloud.de>
2026-08-23 18:50:53 +0200

    Erstelle VSCodium.bash

---
+++ Code-Editor/VSCodium/VSCodium.bash
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# VSCodium installieren
+echo 'VSCodium wird installiert...'
+flatpak install flathub com.vscodium.codium -y
+echo 'VSCodium wurde installiert.'
+echo 'Installation abgeschlossen!'
