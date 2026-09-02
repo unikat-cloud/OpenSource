454cc3b7b547a0edc2e95dc3d69244e6c790e4ec
Unikat-Cloud <dev@unikat-cloud.de>
2026-09-02 08:51:21 +0200

    Entferne alte Vektorigrafikeditor/Inkscape.bash (umbenannt)

--- Vektorigrafikeditor/Inkscape/Inkscape.bash
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Inkscape installieren
-echo 'Inkscape wird installiert...'
-flatpak install flathub org.inkscape.Inkscape -y
-echo 'Inkscape wurde installiert.'
-echo 'Installation abgeschlossen!'
