76edb497118fbe3084694e1707bf2261a97a1191
Unikat-Cloud <dev@unikat-cloud.de>
2026-09-02 08:51:31 +0200

    Entferne alte Vektorigrafikeditor/Inkscape.ps1 (umbenannt)

--- Vektorigrafikeditor/Inkscape/Inkscape.ps1
+++
@@ -1,6 +0,0 @@
-# Installationsskript für Inkscape
-
-Write-Host "Inkscape wird mit Winget installiert..."
-winget install Inkscape.Inkscape -e --source winget --accept-package-agreements --accept-source-agreements
-Write-Host "Inkscape wurde mit Winget installiert."
-Write-Host "Winget Installation abgeschlossen"
