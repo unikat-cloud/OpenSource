70e25731f214e845d0c30fb30d49f8988d0b73e4
Unikat-Cloud <dev@unikat-cloud.de>
2026-09-02 08:50:45 +0200

    Erstelle Vektorgrafikeditor/Inkscape.bash

---
+++ Vektorgrafikeditor/Inkscape/Inkscape.bash
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Inkscape installieren
+echo 'Inkscape wird installiert...'
+flatpak install flathub org.inkscape.Inkscape -y
+echo 'Inkscape wurde installiert.'
+echo 'Installation abgeschlossen!'
